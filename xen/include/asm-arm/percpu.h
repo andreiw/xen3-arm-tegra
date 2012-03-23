05c48e4eb523cc16f9a4c68d111ab67de5704f68
Andrei Warkentin <andrey.warkentin@gmail.com>
2012-03-22 21:38:15 -0400

    Xen: Non-SMP percpu.
    I know I'm lame. I should just make SMP percpu work, but
    I'd rather focus on the FIQ debugger first.
    
    Signed-off-by: Andrei Warkentin <andrey.warkentin@gmail.com>

--- xen/include/asm-arm/percpu.h
+++ xen/include/asm-arm/percpu.h
@@ -1,19 +1,29 @@
 #ifndef __ARM_PERCPU_H__
 #define __ARM_PERCPU_H__
 
+#include <xen/autoconf.h>
+
+#ifdef CONFIG_SMP
 #define PERCPU_SHIFT	13
 #define PERCPU_SIZE		(1UL << PERCPU_SHIFT)
 
 /* Separate out the type, so (int[3], foo) works. */
-#define DEFINE_PER_CPU(type, name)                      \
+#define DEFINE_PER_CPU(type, name)			    \
 	__attribute__((__section__(".data.percpu")))        \
 	__typeof__(type) per_cpu__##name
 
-/* var is in discarded region: offset to particular copy we want */
-#define per_cpu(var, cpu)  \
-	(*RELOC_HIDE(&per_cpu__##var, ((unsigned int)(cpu))<< PERCPU_SHIFT))
-#define __get_cpu_var(var)		per_cpu(var, smp_processor_id()))
+/* FIXME: RELOC_HIDE is not defined. */
 
+/* var is in discarded region: offset to particular copy we want */
+#define per_cpu(var, cpu)						\
+	(*RELOC_HIDE(&per_cpu__##var, ((unsigned int)(cpu))<< PERCPU_SHIFT))
+#else
+#define DEFINE_PER_CPU(type, name)		\
+	__typeof__(type) per_cpu__##name
+#define per_cpu(var, cpu) (per_cpu__##var)
+#endif /* CONFIG_SMP */
+
+#define __get_cpu_var(var)		per_cpu(var, smp_processor_id())
 #define DECLARE_PER_CPU(type, name) extern __typeof__(type) per_cpu__##name
 
 #endif /* __ARM_PERCPU_H__ */
